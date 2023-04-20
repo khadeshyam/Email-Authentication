7787a6daab52dc4ab40ce66d06e091f0fa4a6749
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-04-20 18:01:41 +0530

    hash the body of jwt token

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const jwt = require('jsonwebtoken');
 const cookieParser = require("cookie-parser");
 const {sendOTP,generateOTP} = require("./otp");
+const crypto = require('crypto');
 
 const app = express();
 
@@ -28,23 +29,23 @@ app.get("/verify-otp",(req,res)=>{
 app.post("/signup",async (req,res)=>{
     const {email} = req.body;
     const otp = generateOTP();
+    const hash = crypto.createHash('sha512').update(String(otp)).digest('hex');
     await sendOTP(email,otp);
-    
-    let token = jwt.sign({ otp },process.env.JWT_SECRET_KEY, { algorithm: 'HS512' });
+    let token = jwt.sign({ hash },process.env.JWT_SECRET_KEY, { algorithm: 'HS512' });
     res.cookie("jwtKey",token,{maxAge: 1000*60*5,httpOnly:true});
-
     res.redirect("/verify-otp");
 });
 
 app.post("/verify-otp",(req,res)=>{
-   const {jwtKey} =req.cookies;
+   const {jwtKey} = req.cookies;
 
    if(jwtKey){
     const jwtBody = jwt.verify(jwtKey, process.env.JWT_SECRET_KEY);
-    const userOTP = (req.body.otp);
-    const jwtOTP = String (jwtBody.otp);
+    const otp = req.body.otp;
+    const userOTPHash = crypto.createHash('sha512').update(String(otp)).digest('hex');
+    const jwtOTPHash = String (jwtBody.hash);
  
-    if(userOTP === jwtOTP){
+    if(userOTPHash === jwtOTPHash){
      res.render("success");
     }else{
      res.render("failure");
